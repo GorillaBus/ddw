cbdc4ed6b6ec47fbb2668ae8b51fac1e549c88b1
Edu <eduugr@gmail.com>
2020-02-08 20:47:18 +0100

    Body gravitation and get radius fix
    Gravitation only applies when bodies are not inside each other
    Body.getRadius() now gets the radius from the bounding box model.

--- src/class/Body.js
+++ src/class/Body.js
@@ -4,89 +4,99 @@ const Model = require('./Model');
 
 class Body {
 
-  constructor(settings) {
-    settings = settings || {};
-    settings.x = settings.x || 0;
-    settings.y = settings.y || 0;
+	constructor(settings) {
+		settings = settings || {};
+		settings.x = settings.x || 0;
+		settings.y = settings.y || 0;
 
-    this.scale = settings.scale || 1;
-    this.angle = settings.angle || 0;
-    this.speed = settings.speed || 0;
-    this.mass = settings.mass || 1;
+		this.scale = settings.scale || 1;
+		this.angle = settings.angle || 0;
+		this.speed = settings.speed || 0;
+		this.mass = settings.mass || 1;
 
-    this.acceleration = new Vector({ x: 0, y: 0 });
-    this.velocity = new Vector({ length: this.speed, angle: this.angle });
-    this.location = new Vector({ x: settings.x, y: settings.y });
+		this.acceleration = new Vector({ x: 0, y: 0 });
+		this.velocity = new Vector({ length: this.speed, angle: this.angle });
+		this.location = new Vector({ x: settings.x, y: settings.y });
 
-    // Dimensions
-    this.width = settings.width || 0;
-    this.height = settings.height || 0;
-    this.radius = settings.radius || 0;
+		// Dimensions
+		this.width = settings.width || 0;
+		this.height = settings.height || 0;
+		this.radius = settings.radius || 0;
 
-    this.uuid = Utils.uniqueID();
-    this.visible = settings.visible === false ? false:true;
+		this.uuid = Utils.uniqueID();
+		this.visible = settings.visible === false ? false:true;
 
-    // Last world transform
-    this.worldTransform = {};
+		// Last world transform
+		this.worldTransform = {};
 
-    // Last view transform
-    this.viewTransform = {};
+		// Last view transform
+		this.viewTransform = {};
 
-    // Model
-    this.model = new Model(settings.modelData);
+		// Model
+		this.model = new Model(settings.modelData);
 
-    this.transformToWorld();
-  }
+		this.transformToWorld();
+	}
 
-  update() {
-    this.velocity.addTo(this.acceleration);
-    this.location.addTo(this.velocity);
-    this.acceleration.multiplyBy(0);
-  }
+	update() {
+		this.velocity.addTo(this.acceleration);
+		this.location.addTo(this.velocity);
+		this.acceleration.multiplyBy(0);
+	}
 
-  getWidth() {
-    return this.worldTransform.width;
-  }
+	getRadius() {
+		return this.worldTransform.boundingBox.radius;
+	}
 
-  getHeight() {
-    return this.worldTransform.height;
-  }
+	getWidth() {
+		return this.worldTransform.width;
+	}
 
-  getRadius() {
-    return this.worldTransform.radius;
-  }
+	getHeight() {
+		return this.worldTransform.height;
+	}
 
-  setPosition(vec) {
-    this.location = vec;
-  }
+	setPosition(vec) {
+		this.location = vec;
+	}
 
-  applyForce(force) {
-    let f = force.divide(this.mass);
-    this.acceleration.addTo(f);
-  }
+	applyForce(force) {
+		let f = force.divide(this.mass);
+		this.acceleration.addTo(f);
+	}
 
-  resetVelocity() {
-    this.velocity.multiplyBy(0);
-  }
+	applyNetForce(force) {
+		this.acceleration.addTo(force);
+	}
 
-  transformToWorld() {
-    this.worldTransform = this.model.transform(this.angle, this.scale, this.location);
-  }
+	resetVelocity() {
+		this.velocity.multiplyBy(0);
+	}
 
-  transformToView(view) {
-    this.viewTransform = (this.uuid === view.attachedTo.uuid) ?
-      this.model.transformViewpoint(view, this.angle, this.scale) : this.worldTransform.transformView(view, true);
-  }
+	transformToWorld() {
+		this.worldTransform = this.model.transform(this.angle, this.scale, this.location);
+	}
 
-  gravitateTo(target, gravityFactor) {
-    const diff = target.location.substract(this.location);
-    const distance = diff.getLength();
-    const force = this.mass * target.mass / (distance * distance);
-    diff.normalize();
-    diff.multiplyBy(force);
-    this.velocity.addTo(diff);
-    return force;
-  }
+	transformToView(view) {
+		this.viewTransform = (this.uuid === view.attachedTo.uuid) ?
+		this.model.transformViewpoint(view, this.angle, this.scale) : this.worldTransform.transformView(view, true);
+	}
+
+	gravitateTo(target, gravityFactor) {
+		const diff = target.location.substract(this.location);
+		const distance = diff.getLength();
+		const minDistance = target.getRadius() + this.getRadius();
+
+		if (minDistance > distance) {
+			return;
+		}
+
+		const force = this.mass * target.mass / (distance * distance);
+		diff.normalize();
+		diff.multiplyBy(force);
+		this.velocity.addTo(diff);
+		return force;
+	}
 
 }
 
