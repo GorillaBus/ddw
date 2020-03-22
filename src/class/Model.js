ead53c4c921653179cab525a1008219be861ceb9
Edu <eduugr@gmail.com>
2020-03-22 23:48:27 +0100

    Model: added center and bounds computation

--- src/class/Model.js
+++ src/class/Model.js
@@ -1,16 +1,42 @@
-const Vector = require('./Vector');
-
 class Model {
 
 	constructor(settings) {
     this.points = settings.points || [];
 		this.strokeColor = settings.strokeColor || "#94ec5d";
     this.fillColor = settings.fillColor || null;
+		this.bounds = this.getBounds();
+		this.center = this.getCenter();
+		this.width = this.bounds.xMax - this.bounds.xMin;
+		this.height = this.bounds.yMax - this.bounds.yMin;
+		this.radius = Math.max(this.width, this.height) / 2;
 	}
 
   getPoints() {
     return this.points;
   }
+
+	getBounds() {
+		const xs = [];
+		const ys = [];
+		for (let i=0,len=this.points.length; i<len; i++) {
+			const point = this.points[i];
+			xs.push(point[0]);
+			ys.push(point[1]);
+		}
+		return {
+			xMin: Math.min.apply(null, xs),
+			xMax: Math.max.apply(null, xs),
+			yMin: Math.min.apply(null, ys),
+			yMax: Math.max.apply(null, ys)
+		};
+	}
+
+	getCenter() {
+		return [
+			(this.bounds.xMin + this.bounds.xMax) / 2,
+			(this.bounds.yMin + this.bounds.yMax) / 2
+		];
+	}
 
   transform(r, s, t) {
 		t = t || [0, 0];
