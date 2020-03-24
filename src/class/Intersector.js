3ec2c06e0e26993ba4e212625318daaa943eee14
Edu <eduugr@gmail.com>
2020-03-24 03:30:03 +0100

    Intersector: Removed unnecessary shaping methods.

--- src/class/Intersector.js
+++ src/class/Intersector.js
@@ -1,39 +1,13 @@
 class Intersector {
 
-	getBoundingRectangle(model) {
-		return {
-			x: model.bounds.xMin,
-			y: model.bounds.yMin,
-			width: model.width,
-			height: model.height
-		};
-	}
-
-	getBoundingCircle(model) {
-		return {
-			x: model.center[0],
-			y: model.center[1],
-			radius: model.radius
-		};
-	}
-
-  rangeIntersect(min0, max0, min1, max1) {
-    return  Math.max(min0, max0) >= Math.min(min1, max1) &&
-      Math.min(min0, max0) <= Math.max(min1, max1);
+  rectangleRectangle(r1, r2) {
+    return this.rangeIntersect(r1.bounds.xMin, r1.bounds.xMin + r1.width, r2.bounds.xMin, r2.bounds.xMin + r2.width) &&
+      this.rangeIntersect(r1.bounds.yMin, r1.bounds.yMin + r1.height, r2.bounds.yMin, r2.bounds.yMin + r2.height);
   }
 
-  rectangleInRectangle(rectangleModel1, rectangleModel2) {
-		const r1 = this.getBoundingRectangle(rectangleModel1);
-		const r2 = this.getBoundingRectangle(rectangleModel2);
-    return this.rangeIntersect(r1.x, r1.x + r1.width, r2.x, r2.x + r2.width) &&
-      this.rangeIntersect(r1.y, r1.y + r1.height, r2.y, r2.y + r2.height);
-  }
-
-	circleInRectangle(circleModel, rectangleModel) {
-		const c = this.getBoundingCircle(circleModel);
-		const r = this.getBoundingRectangle(rectangleModel);
-		const distX = Math.abs(c.x - r.x - r.width / 2);
-		const distY = Math.abs(c.y - r.y - r.height / 2);
+	circleRectangle(c, r) {
+		const distX = Math.abs(c.center[0] - r.bounds.xMin - r.width / 2);
+		const distY = Math.abs(c.center[1] - r.bounds.yMin - r.height / 2);
 		// Out of range
 		if (distX > (r.width / 2 + c.radius) || distY > (r.height / 2 + c.radius)) {
 			return false;
@@ -48,22 +22,22 @@ class Intersector {
 		return (dx * dx + dy * dy <= (c.radius * c.radius));
 	}
 
-	circleCircle(circleModel1, circleModel2) {
-		const c1 = this.getBoundingCircle(circleModel1);
-		const c2 = this.getBoundingCircle(circleModel2);
-		// Get the Distance vector
-		const xDist = c1.x - c2.x;
-		const yDist = c1.y - c2.y;
-		// We'll save a Math.sqrt() to verify distances like this:
+	circleCircle(c1, c2) {
+		const xDist = c1.center[0] - c2.center[0];
+		const yDist = c1.center[1] - c2.center[1];
 		const distSquared = (xDist * xDist) + (yDist * yDist);
 		const radiusSquared = (c1.radius + c2.radius) * (c1.radius + c2.radius);
-		// Collision check
 		if (distSquared < radiusSquared) {
 			return { x: xDist, y: yDist, dist_squared: distSquared };
 		}
 		return false;
 	}
 
+  rangeIntersect(min0, max0, min1, max1) {
+    return  Math.max(min0, max0) >= Math.min(min1, max1) &&
+      Math.min(min0, max0) <= Math.max(min1, max1);
+  }
+
 }
 
-module.exports = new Intersector();
+module.exports = Intersector;
