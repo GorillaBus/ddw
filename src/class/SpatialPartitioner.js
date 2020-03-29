4620fc5157b28ba2f1bcbda097e4963f0a67d621
Edu <eduugr@gmail.com>
2020-03-30 01:51:20 +0200

    SpatialPartitioner: refactored registerBody and added getNeighboarCells method

--- src/class/SpatialPartitioner.js
+++ src/class/SpatialPartitioner.js
@@ -6,6 +6,8 @@ class SpatialPartitioner {
 		this.cells = [];
 	}
 
+  run() { }
+
 	getCell(cellId) {
 		for (let i=0,len=this.cells.length; i<len; i++) {
 			if (this.cells[i].id === cellId) {
@@ -18,6 +20,28 @@ class SpatialPartitioner {
 	getCells() {
 		return this.cells;
 	}
+
+  getNeighborCells(cell) {
+    const neighborCells = [];
+    const neighborIds = [
+      [cell.x - 1, cell.y - 1].join("_"),
+      [cell.x, cell.y - 1].join("_"),
+      [cell.x + 1, cell.y - 1].join("_"),
+      [cell.x + 1, cell.y].join("_"),
+      [cell.x + 1, cell.y + 1].join("_"),
+      [cell.x, cell.y + 1].join("_"),
+      [cell.x - 1, cell.y + 1].join("_"),
+      [cell.x - 1, cell.y].join("_")
+    ];
+    for (let i=0; i<8; i++) {
+      const id = neighborIds[i];
+      const neighbor = this.getCell(id);
+      if (neighbor) {
+        neighborCells.push(neighbor);
+      }
+    }
+    return neighborCells;
+  }
 
 	addCell(cellData) {
     const cell = {
@@ -32,45 +56,23 @@ class SpatialPartitioner {
 		this.cells = [];
 	}
 
-	run() { }
+  registerBody(body, center) {
+    const point = body.world.getCenter();
+    const cellData = this.pointPosition(point);
 
-	registerBody(body) {
-		const boundingRect = body.getBoundingRect();
+    // Create new cell if required
+    let cell = this.getCell(cellData.id);
+    if (!cell) {
+      cell = this.addCell(cellData);
+    }
 
-		// Find grid positions for each point
-    const currentCells = [];
-		for (let i=0,len=boundingRect.length; i<len; i++) {
-			const point = boundingRect[i];
-
-			// Get point's cell position on the grid
-			const cellData = this.pointPosition(point);
-
-      // Find if the cell has already been registered
-      let found = false;
-      for (let e=0,len=currentCells.length; e<len; e++) {
-        if (currentCells[e] === cellData.id) {
-          found = true;
-          break;
-        }
-      }
-      if (found) continue;
-
-			// Create new cell if required
-			let cell = this.getCell(cellData.id);
-			if (!cell) {
-				cell = this.addCell(cellData);
-			}
-
-      // Add body in cell's collection
-			cell.bodies.push(body);
-      currentCells.push(cellData.id);
-		}
-	}
+    // Add body in cell's collection
+    cell.bodies.push(body);
+  }
 
 	pointPosition(point) {
 		const xComponent = Math.floor(point[0] / this.cellSize);
 		const yComponent = Math.floor(point[1] / this.cellSize);
-
     return {
       x: xComponent,
       y: yComponent,
