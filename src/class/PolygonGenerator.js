38076e596d76608e654185be0bc97a7def978a14
Edu <eduugr@gmail.com>
2020-04-11 22:37:19 +0200

    PolygonGenerator: now returns an array of points instead of a Model instance
    - Gradient

--- src/class/PolygonGenerator.js
+++ src/class/PolygonGenerator.js
@@ -1,30 +1,20 @@
-const Model = require("./Model");
-
 class PolygonGenerator {
 
   circle(settings) {
     const divs = settings.divs || 32;
-    const fillColor = settings.fillColor || '#FFFFFF';
-    const strokeColor = settings.strokeColor || null;
     const radius = settings.radius || 1;
     const angleFraction = (Math.PI * 2) / settings.divs;
     const points = [];
     for (let i=0; i<divs; i++) {
       const point = [
-        Math.cos(angleFraction * i+1) * radius,
+        Math.cos(angleFraction * i + 1) * radius,
         Math.sin(angleFraction * i + 1) * radius,
       ];
       points.push(point);
     }
-    return new Model({
-      fillColor,
-      fillColor,
-      points
-    });
+    return points;
   }
 
 }
 
-const itc = new PolygonGenerator();
-
-module.exports = itc;
+module.exports = new PolygonGenerator();
